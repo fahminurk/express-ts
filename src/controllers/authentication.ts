f3a1860d7b5ff0ae7ed3c734c5ef89bf4e43c618
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-03 18:31:06 +0700

    update controller auth

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -28,3 +28,40 @@ export const register = async (req: express.Request, res: express.Response) => {
     return res.sendStatus(400);
   }
 };
+
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.status(400).json({ message: "email and password required" });
+
+    const user = await getUserByEmail(email).select(
+      "+authentication.salt +authentication.password"
+    );
+
+    if (!user) return res.status(400).json({ message: "user not found" });
+
+    const hashPassword = authentication(user.authentication.salt, password);
+
+    if (user.authentication.password !== hashPassword)
+      return res.status(400).json({ message: "Wrong password" });
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(
+      salt,
+      user._id.toString()
+    );
+
+    await user.save();
+
+    res.cookie("token", user.authentication.sessionToken, {
+      domain: "localhost",
+      path: "/",
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
