01a3871afc548f14f4618b97ef0b79fa7bb905a5
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-03 18:31:57 +0700

    update route auth

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,6 +1,8 @@
-import { register } from "../controllers/authentication";
+import { validateLogin, validateRegister } from "../middlewares/validation";
+import { login, register } from "../controllers/authentication";
 import express from "express";
 
 export default (router: express.Router) => {
-  router.post("/auth/register", register);
+  router.post("/auth/register", validateRegister, register);
+  router.post("/auth/login", validateLogin, login);
 };
